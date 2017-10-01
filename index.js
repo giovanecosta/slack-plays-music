599ecab22338d9f7f50ac2641760f402378c11c1
giovane <giovane.tomaz@redealumni.com>
2017-10-01 20:43:00 -0300

    Get note and duration from a word

--- index.js
+++ index.js
@@ -10,12 +10,56 @@ module.exports = function() {
 
     var Tone = require('tone');
 
-    SPM.play = function() {
-      //a 4 voice Synth
-      var polySynth = new Tone.PolySynth(4, Tone.Synth).toMaster();
-      //play a chord
-      polySynth.triggerAttackRelease(["C4", "E4", "G4", "B4"], "2n");
+    SPM.play = function(text, channel) {
 
+      var synth = SPM.getInstrumentForChannel(channel);
+
+      var note = SPM.getNoteFromText(text);
+
+      var time = SPM.getSustainFromText(text);
+
+      synth.triggerAttackRelease(note, time);
+
+    }
+
+    SPM.getInstrumentForChannel = function(channel) {
+      return new Tone.Synth({
+        "oscillator" : {
+          "type" : "pwm",
+          "modulationFrequency" : 0.2
+        },
+        "envelope" : {
+          "attack" : 0.02,
+          "decay" : 0.1,
+          "sustain" : 0.2,
+          "release" : 0.9,
+        }
+      }).toMaster();
+    }
+
+    SPM.getNoteFromText = function(text) {
+      var notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+      var note = 'C';
+      var octave = 1;
+      var maxLength = notes.length * 5;
+
+      var charCode = -1; // chr('!') == 33
+
+      for (var l of text.split('')) {
+        charCode += l.charCodeAt(0) - 32;
+      }
+
+      charCode = (charCode % maxLength);
+      octave = Math.floor(charCode / notes.length) + 1;
+      note = notes[charCode % notes.length];
+
+      console.log(note + octave);
+
+      return note + octave;
+    }
+
+    SPM.getSustainFromText = function(text) {
+      return text.length * 0.1;
     }
 
     return SPM;
