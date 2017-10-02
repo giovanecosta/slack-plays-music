57cdff5f1e7096cb8ac3f7e692f97cff02a28d71
giovane <giovane.tomaz@redealumni.com>
2017-10-02 02:38:40 -0300

    Connection with slack ok!

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 var WebSocketServer = require('websocket').server;
-const server = require('node-http-server');
+var server = require('node-http-server');
+var uid = require('uid');
+
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+var bot_token = process.env.SLACK_BOT_TOKEN || '';
+
+var rtm = new RtmClient(bot_token);
+
+var channel;
+
+var connections = {};
 
 var httpServer = new server.Server({
   port: 8000,
@@ -23,17 +36,46 @@ wsServer = new WebSocketServer({
 // WebSocket server
 wsServer.on('request', function(request) {
   var connection = request.accept(null, request.origin);
+  connection.uid = uid(24);
+  connections[connection.uid] = connection;
 
-  setInterval(function(){
-    sendActivity(connection)
-  }, 500);
-
+  connection.on('close', function(){
+    delete connections[connection.uid];
+  })
 });
 
-function sendActivity(connection) {
-  connection.sendUTF(JSON.stringify({text: 'affwfwew wefwefwefwafewf wFFEFWFF', channel: ''}));
+function sendActivity(text, channel) {
+  var count = 0;
+  for (var id in connections){
+    connections[id].sendUTF(JSON.stringify({text: text, channel: channel}));
+    count++;
+  }
+  console.log('Broadcasted for ' + count + ' connections.');
 }
 
 function serverReady(server){
-   console.log( `Server on port ${server.config.port} is now up`);
+  console.log( 'Server on port ' + server.config.port + 'is now up');
 }
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+  console.log('Logged in as ' + rtmStartData.self.name + ' of team ' + rtmStartData.team.name);
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, function (messageData) {
+  var origin = false;
+  var channel = rtm.dataStore.getChannelById(messageData.channel);
+  var dm = rtm.dataStore.getDMById(messageData.channel);
+
+  if (channel) {
+    origin = channel.name;
+  } else if (dm) {
+    origin = 'direct';
+  }
+
+  if (origin) {
+    sendActivity(messageData.text, origin);
+  }
+
+});
+
+rtm.start();
