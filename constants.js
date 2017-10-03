d1874e28ab62f7136e9e8f8eb99f5de03f8a24e9
giovane <giovane.tomaz@redealumni.com>
2017-10-03 01:51:17 -0300

    Adds constants file

---
+++ constants.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  STATIC_MESSAGES: {
+    PRESENCE_ACTIVE: '__presence_active',
+    PRESENCE_AWAY: '__presence_away'
+  },
+  SPECIAL_ORIGINS: {
+    DIRECT: 'direct',
+    PRESENCE_CHANGE: 'presence_change'
+  }
+};
