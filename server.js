b0708880e3f4498db47b52cd0d73d716359d8ffb
giovane <giovane.tomaz@redealumni.com>
2017-10-03 01:51:36 -0300

    Listen to status change

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var WebSocketServer = require('websocket').server;
 var server = require('node-http-server');
 var uid = require('uid');
+var STATIC_MESSAGES = require('./constants').STATIC_MESSAGES;
+var SPECIAL_ORIGINS = require('./constants').SPECIAL_ORIGINS;
 
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
@@ -65,14 +67,12 @@ rtm.on(RTM_EVENTS.MESSAGE, function (messageData) {
   var origin = false;
   var channel = rtm.dataStore.getChannelById(messageData.channel);
   var dm = rtm.dataStore.getDMById(messageData.channel);
-  var user = rtm.dataStore.getUserById(messageData.user);
-  var bot = rtm.dataStore.getBotById(messageData.user);
-  var who = user || bot;
+  var who = getUserOrBot(messageData.user);
 
   if (channel) {
     origin = channel.name;
   } else if (dm) {
-    origin = 'direct';
+    origin = SPECIAL_ORIGINS.DIRECT;
   }
 
   if (origin && who) {
@@ -81,4 +81,29 @@ rtm.on(RTM_EVENTS.MESSAGE, function (messageData) {
 
 });
 
+rtm.on(RTM_EVENTS.PRESENCE_CHANGE, function(presenceData) {
+  var userIds = presenceData.users;
+  if(presenceData.user) {
+    userIds = [presenceData.user];
+  }
+
+  var message = (presenceData.presence == 'active' ? STATIC_MESSAGES.PRESENCE_ACTIVE : STATIC_MESSAGES.PRESENCE_AWAY);
+
+  for(var userId of userIds) {
+    var who = getUserOrBot(userId);
+
+    if (who) {
+      sendActivity(message, SPECIAL_ORIGINS.PRESENCE_CHANGE, who.name);
+    }
+  }
+});
+
+
+
 rtm.start();
+
+function getUserOrBot(id) {
+  var user = rtm.dataStore.getUserById(id);
+  var bot = rtm.dataStore.getBotById(id);
+  return user || bot;
+}
