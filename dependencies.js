0889298e6b893a4e19457fd8b3108bc01b458acc
giovane <giovane.tomaz@redealumni.com>
2017-10-04 17:48:42 -0300

    Add some instruments

--- dependencies.js
+++ dependencies.js
@@ -1,29 +1,19 @@
 'use strict';
 
-var Tone = require('tone');
 var Instrument = require('./instrument.js');
-
-var standard = new Tone.Synth();
-
-var whatever = new Tone.Synth({
-  "oscillator" : {
-    "type" : "pwm",
-    "modulationFrequency" : 0.9
-  },
-  "envelope" : {
-    "attack" : 0.9,
-    "decay" : 0.7,
-    "sustain" : 0.9,
-    "release" : 0.2,
-  }
-});
+var INSTRUMENTS = require('./instruments.js');
 
 module.exports = {
   SPM: require('./index.js'),
   WebSocketClient: require('./websocket_client.js'),
   DrawAdapter: require('./draw_adapter.js'),
   Instruments: {
-    standard: new Instrument(standard)
-    //whatever: new Instrument(whatever)
+    whatever: new Instrument(INSTRUMENTS.whatever),
+    treeTrunk: new Instrument(INSTRUMENTS.treeTrunk),
+    steelPan: new Instrument(INSTRUMENTS.steelPan),
+    delicate: new Instrument(INSTRUMENTS.delicate),
+    harmonic: new Instrument(INSTRUMENTS.harmonic),
+    eletricCello: new Instrument(INSTRUMENTS.eletricCello),
+    kalimba: new Instrument(INSTRUMENTS.kalimba)
   }
 }
